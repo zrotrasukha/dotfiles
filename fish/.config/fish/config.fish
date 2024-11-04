79aa90a5f7495f6f12571d21d8263b578000eb63
zrotrasukha <zrotrasukha@gmail.com>
2024-11-05 02:48:13 +0530

    added fish files

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,1 +1,171 @@
-../../dotfiles/fish/.config/fish/config.fish
+# Vi mode for Fish
+set -g fish_greeting
+
+fish_vi_key_bindings
+
+# Source global definitions (Fish equivalent)
+if test -f /etc/fish/config.fish
+    source /etc/fish/config.fish
+end
+
+# Set up PATH if not already included
+if not contains "$HOME/.local/bin" $PATH
+    set -gx PATH $HOME/.local/bin $HOME/bin $HOME/scripts $HOME/.console-ninja/.bin $PATH
+end
+
+# Uncomment the following line if you don't like systemctl's auto-paging feature
+# set -gx SYSTEMD_PAGER ""
+
+# Source all .fish files in ~/.config/fish/conf.d/ if directory exists
+if test -d ~/.config/fish/conf.d
+    for rc in ~/.config/fish/conf.d/*.fish
+        source $rc
+    end
+end
+
+# yazi config (Fish function)
+function yy
+    set -l tmp (mktemp -t "yazi-cwd.XXXXXX")
+    yazi $argv --cwd-file "$tmp"
+    set -l cwd (cat "$tmp")
+    if test -n "$cwd" -a "$cwd" ! "$PWD"
+        cd "$cwd"
+    end
+    rm -f "$tmp"
+end
+
+# Homebrew environment setup
+if test -d ~/.linuxbrew
+    eval ("/home/linuxbrew/.linuxbrew/bin/brew shellenv")
+end
+
+# NVM initialization
+set -gx NVM_DIR "$HOME/.nvm"
+set -gx PATH $PATH $PWD/lua_env/bin
+set -gx TREESITTER_DIR ~/.local/share/nvim/site/pack/packer/start/nvim-treesitter/parser
+
+# Aliases
+alias fs "nvim ~/.config/fish/config.fish"
+alias y yazi
+alias blueon "hyprshade on blue-light-filter"
+alias bo "hyprshade on blue-light-filter"
+alias blueoff "hyprshade off"
+alias bf "hyprshade off"
+alias wifi "nmcli dev wifi"
+alias wifir "nmcli radio wifi"
+alias bt bluetoothctl
+alias q exit
+function bton
+    bluetoothctl power on
+end
+function btoff
+    bluetoothctl power off
+end
+alias snips "nvim /home/zrotrasukha/dotfiles/nvim/.config/nvim/lua/config/luasnip.lua"
+alias dots "cd ~/dotfiles"
+set -gx STARSHIP_CONFIG "$HOME/.config/starship/starship.toml"
+eval (starship init fish)
+alias sb "source ~/.config/fish/config.fish"
+alias v nvim
+alias lss "pactl list short sinks"
+alias c clear
+alias ccp "cliphist wipe"
+alias rd "hyprctl reload"
+alias sus "systemctl suspend"
+alias ncs "nmcli connection show"
+alias open xdg-open
+alias cfp "pwd | wl-copy"
+
+# Tmux Aliases
+alias t tmux
+alias ta "tmux attach-session"
+alias tns "tmux new-session -s"
+alias tks "tmux kill-session -t"
+alias tka "tmux kill-server"
+alias tas "tmux attach-session -t"
+
+# Coding files
+alias way "nvim ~/.config/waybar"
+alias b "nvim ~/.config/fish/config.fish"
+alias plugs "nvim ~/.config/nvim/lua/plugins/"
+alias conf "nvim ~/.config/hypr/hyprland.conf"
+alias obs "nvim /home/zrotrasukha/MEGA/MEGA/Knowledge_and_Info"
+alias blue "nvim ~/.config/hypr/shaders/blue-light-filter.glsl"
+alias 100 "cd /home/zrotrasukha/MEGA/MEGA/100xDev"
+
+# fzf Aliases
+alias f 'cd (fzf --preview "bat --color always {}")'
+alias fn 'nvim (fzf --preview "bat --color always {}")'
+alias fy 'yazi (fzf --preview "bat --color always {}")'
+
+# Website Aliases
+alias 100x 'xdg-open https://app.100xdevs.com/courses/14'
+alias yt 'xdg-open https://www.youtube.com'
+alias amigo "xdg-open https://amigo.amityonline.com/my/"
+alias mdb "xdg-open https://www.mongodb.com/docs/manual/tutorial/install-mongodb-community-with-docker/"
+
+# Docker-related Aliases and Functions
+alias dps "sudo docker ps"
+function drun
+    set -l name (or $argv[1] "mongodb")
+    set -l port (or $argv[2] "27017")
+    set -l filePath (or $argv[3] "$HOME/mongoExport")
+    sudo docker run --name "$name" -v "$filePath":/data -p "$port":27017 -d mongo:latest
+end
+function darn
+    set -l name (or $argv[1] "mongodb")
+    sudo docker run --rm --name "$name" -it mongo mongosh "mongodb+srv://cluster0.7xf16.mongodb.net/" --apiVersion 1 --username zrotrasukha
+end
+function dexp
+    set -l dbName (or $argv[1] "test")
+    set -l collectionName (or $argv[2] "users")
+    set -l fileName (or $argv[3] "$collectionName.json")
+    sudo docker run --rm -v (pwd):/data -it mongo bash -c "mongoexport --uri 'mongodb+srv://cluster0.7xf16.mongodb.net/$dbName' --collection $collectionName --out /data/$fileName --jsonArray --username zrotrasukha"
+end
+alias db "cd ~/dockstore"
+function dimp
+    set -l dbName (or $argv[1] "test")
+    set -l collectionName (or $argv[2] "users")
+    set -l fileName (or $argv[3] "$collectionName.json")
+    sudo docker run --rm -v (pwd):/data -it mongo bash -c "mongoimport --uri 'mongodb+srv://cluster0.7xf16.mongodb.net/$dbName' --collection $collectionName --file /data/$fileName --jsonArray --username zrotrasukha"
+end
+function msh
+    set -l name (or $argv[1] "mongodb")
+    set -l port (or $argv[2] "27017")
+    sudo docker exec -it "$name" mongosh
+end
+function drm
+    set -l session (or $argv[1] "mongodb")
+    sudo docker rm -f "$session"
+end
+function frm
+    set -l appName (or $argv[1] "")
+    rm ~/.local/share/applications/"$appName".desktop
+end
+
+# HTTPie function
+function hl
+    set -l method (or $argv[1] "GET")
+    set -l port (or $argv[2] "3000")
+    set -l endPoints (or $argv[3] "")
+    http "$method" http://localhost:"$port"/"$endPoints"
+end
+
+# nvm initialization for Fish
+if test -d "$HOME/.nvm"
+    bass source $HOME/.nvm/nvm.sh --no-use
+end
+
+# Optionally, for nvm bash completion
+if test -f "$HOME/.nvm/bash_completion"
+    bass source $HOME/.nvm/bash_completion
+end
+
+
+set -gx EDITOR nvim
+set -gx VISUAL nvim
+
+# bun
+set -gx BUN_INSTALL "$HOME/.bun"
+set -gx PATH $BUN_INSTALL/bin $PATH
+set -gx PATH $PATH /home/zrotrasukha/.spicetify
